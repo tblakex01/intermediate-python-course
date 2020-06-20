e6e87eb7108382af1fcb6a95a91ba02317ee9681
Nizda <nizda@nizda.net>
2020-06-20 16:12:39 -0400

    1

--- dice_roller.py
+++ dice_roller.py
@@ -7,7 +7,12 @@ def main():
     for i in range(0, dice_rolls):
         roll = random.randint(1, 6)
         dice_sum += roll
-        print(f'You rolled a {roll}')
+        if roll == 1:
+            print(f'You rolled a {roll}! Critical Fail')
+        elif roll == 6:
+            print(f'You rolled a {roll}! Critical Success!')
+        else:
+            print(f'You rolled a {roll}')
 
 
 if __name__ == "__main__":
